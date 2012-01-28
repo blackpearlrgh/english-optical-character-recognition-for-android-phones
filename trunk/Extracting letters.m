cf9e362728c1fb847bd5bf7a508ba0122b8268c1
hendelqady@gmail.com <hendelqady@gmail.com@c728a7a6-43e3-89fd-11cb-5c7905eb2401>
2012-01-28 14:34:00 +0000

    Extracting letters

---
+++ trunk/Extracting letters.m
@@ -0,0 +1,32 @@
+
+
+clc, close all
+imagen=imread('abcCapital.jpg');
+imshow(imagen);
+imagen=rgb2gray(imagen);
+imagen =~im2bw(imagen,graythresh(imagen));
+imagen = bwareaopen(imagen,30);
+re=imagen;
+while 1
+    %Fcn 'lines' separate lines in text
+    [fl re]=lines(re);
+    imgn=fl;
+    %Uncomment line below to see lines one by one
+    %imshow(fl);pause(0.5)    
+    %-----------------------------------------------------------------     
+    % Label and count connected components
+    [L Ne] = bwlabel(imgn);    
+    for n=1:Ne
+        [r,c] = find(L==n);
+        % Extract letter
+        n1=imgn(min(r):max(r),min(c):max(c));  
+        figure, imshow(n1);
+       
+    end
+    %*When the sentences finish, breaks the loop
+    if isempty(re)  %See variable 're' in Fcn 'lines'
+        break
+    end    
+end
+
+clear all

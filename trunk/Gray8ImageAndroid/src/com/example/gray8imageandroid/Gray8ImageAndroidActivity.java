b6d58669841c025692a77085d7014ebaefb5b498
nashwa.ahmed89 <nashwa.ahmed89@c728a7a6-43e3-89fd-11cb-5c7905eb2401>
2012-01-29 00:58:43 +0000

    test for JJIL Android library

---
+++ trunk/Gray8ImageAndroid/src/com/example/gray8imageandroid/Gray8ImageAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.example.gray8imageandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gray8ImageAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

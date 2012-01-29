64f700f32c85296fad3246301e50dad8600c7acd
nashwa.ahmed89 <nashwa.ahmed89@c728a7a6-43e3-89fd-11cb-5c7905eb2401>
2012-01-29 01:03:32 +0000

    --

--- trunk/Gray8ImageAndroid/src/com/example/gray8imageandroid/Gray8ImageAndroidActivity.java
+++ trunk/Gray8ImageAndroid/src/com/example/gray8imageandroid/Gray8ImageAndroidActivity.java
@@ -1,7 +1,18 @@
 package com.example.gray8imageandroid;
 
 import android.app.Activity;
+import jjil.core.Gray8Image;
 import android.os.Bundle;
+
+public class Gray8ImageAndroid extends Gray8Image {
+
+	public Gray8ImageAndroid(int cWidth, int cHeight, byte bValue) {
+		super(cWidth, cHeight, bValue);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
 
 public class Gray8ImageAndroidActivity extends Activity {
     /** Called when the activity is first created. */
